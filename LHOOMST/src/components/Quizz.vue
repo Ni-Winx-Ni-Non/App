39012846ba21df1465a5e3f9e7d15f4dc030ad9d
erygn <etienne.reygner@gmail.com>
2022-12-01 21:05:08 +0100

    Exemple

---
+++ LHOOMST/src/components/Quizz.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    data() {
+        return {
+            questions: [
+                {id: 1, title: 'MST', description: "Qu'est ce que c'est ?"},
+                {id: 2, title: 'IST', description: "Qu'est ce que c'est ?"},
+            ]
+        }
+    },
+    methods: {
+        valid(id) {
+            console.log("Question id: " + id)
+            this.$emit('emitValid', {index: id})
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div v-for="(item, index) in questions" :key="index">
+            <h2>{{ item.title }}</h2>
+            <p>{{ item.description }}</p>
+            <button @click="valid(index)">Valider</button>
+        </div>
+    </div>
+</template>
