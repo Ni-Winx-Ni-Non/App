d2ab01d1614e7f358d1b3ba5f413ec3f7e164e58
alibertangelo <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>
2022-12-02 02:57:16 +0100

    Quiz modifications

--- LHOOMST/src/components/Quizz.vue
+++ LHOOMST/src/components/Quizz.vue
@@ -1,35 +1,34 @@
 <script>
+import questionList from "../assets/questions-quiz.json"
+
 export default {
     data() {
         return {
-            questions: [
-                { id: 1, description: "Si je ne vais pas jusqu’à la pénétration, il est impossible que j’attrape une IST.", response: false, explication:"Certaines IST peuvent être « manuportées », c’est-à-dire transmises par le biais des mains, quand il y a échange ou contact de fluides (sécrétions vaginales ou sperme)." },
-                { id: 2, description: "Si mon test VIH est négatif, je suis aussi négatif aux autres IST.", response: false , explication:"Le test VIH ne dépiste pas les autres IST !"},
-                { id: 3, description: "Si mon test VIH est négatif, je suis aussi négatif aux autres IST.", response: false , explication:"Le test VIH ne dépiste pas les autres IST !"},
-                { id: 4, description: "Si mon test VIH est négatif, je suis aussi négatif aux autres IST.", response: false , explication:"Le test VIH ne dépiste pas les autres IST !"},
-                { id: 5, description: "Si mon test VIH est négatif, je suis aussi négatif aux autres IST.", response: false , explication:"Le test VIH ne dépiste pas les autres IST !"},
-            ],
+            questions: questionList,
             questionId: 0,
             correctAnswers: 0,
             questionAnswered: false,
-            wellAnswered:false,
-            gameOver:false
+            wellAnswered: true,
+            gameOver: false
         }
     },
     methods: {
         questionSuivante() {
+            this.wellAnswered = true
             this.questionAnswered = false
             this.questionId++
         },
-        valid(id) {
-            console.log("Question id: " + id)
-            this.$emit('emitValid', { index: id })
+        valid() {
+            console.log("Question id: " + this.questionId)
+            this.$emit('emitValid', { index: this.questionId })
         },
         answer(bool) {
+            console.log(this.questionAnswered)
             if (this.questions[this.questionId].response == bool) {
                 this.wellAnswered = true
                 console.log('Win')
                 this.correctAnswers++
+
                 console.log(this.correctAnswers)
             } else {
                 this.wellAnswered = false
@@ -37,64 +36,89 @@ export default {
             }
             this.questionAnswered = true
         },
-        end(){
-            this.gameOver =true;
+        end() {
+            this.gameOver = true;
+        },
+        reset() {
+
+            this.questionId = 0,
+                this.correctAnswers = 0,
+                this.questionAnswered = false,
+                this.wellAnswered = true,
+                this.gameOver = false,
+                console.log('Quizz destroyed')
         }
+
     }
 }
+
 </script>
 
 <template>
-    <div>
+    <!-- Questions: on affiche une div pour chaque question -->
+    <div v-if="!gameOver" class="playing" :class="this.wellAnswered  ? ''  : 'wrong'">
+        <div class="card">
+            <h1 style="margin-bottom: 0px">Question {{ (questionId + 1) }} / 5</h1>
 
-        <!-- Questions: on affiche une div pour chaque question -->
-        <div v-if="!gameOver">
-            <h1>Question {{( questionId +1)}} / 5</h1>
-            <h2>{{ questions[questionId].description }}</h2>
-
-            
-            <div v-if="!questionAnswered" class="boutons">
-                <button class="vrai-faux" @click="answer(true)">Vrai</button>
-                <button class="vrai-faux" @click="answer(false)">Faux</button>
+            <div v-if="questionAnswered" class="content">
+                <div>
+                    <h1 style="margin-top: 0px" v-if="this.wellAnswered"> Bonne réponse !</h1>
+                    <h1 v-else style="color:red; margin-top: 0px"> Mauvaise réponse :(</h1>
+                    <h2>{{ questions[questionId].explication }}</h2>
+                </div>
             </div>
-
-            <div v-else>
-                <h1 v-if="this.wellAnswered"> Bonne réponse !</h1>
-                <h1 v-else style="color:red"> Mauvaise réponse :(</h1>
-                <h2>{{ questions[questionId].explication }}</h2>
-
-                <button v-if="(questionId < questions.length -1)" @click="questionSuivante">Suivant</button>
-                <button v-else @click="end">Voir le score</button>
-
+            <div class="content" v-else>
+                <h2>{{ questions[questionId].description }}</h2>
             </div>
-
-
         </div>
 
-        <div v-else>
-            <h1>Vous avez obtenu un score de {{this.correctAnswers}} / 5</h1>
+        <div v-if="!questionAnswered" class="boutons">
+            <button @click="answer(true)">Vrai</button>
+            <button @click="answer(false)">Faux</button>
+        </div>
+
+        <div v-else class="boutons">
+            <button v-if="(questionId < 5 - 1)" @click="questionSuivante">Suivant</button>
+            <button v-else @click="end">Voir le score</button>
+        </div>
+
+
+    </div>
+
+    <div v-else class="score">
+        <h1>Vous avez obtenu un score de {{ this.correctAnswers }} / 5</h1>
+        <div class="boutons">
+            <button @click="($emit('endQuizz', { result: correctAnswers }), reset())">Continuer</button>
         </div>
 
     </div>
+
 </template>
 
 <style scoped>
-
-
 h1 {
- margin : 2rem;
+    margin: 2rem;
+    text-align: center;
 }
 
+.score{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+}
 
 button {
     background-color: #3C7196;
     color: white;
     border: 2px solid #3C7196;
-    padding: 0.85rem;
+    padding: 1rem 5rem;
     border-radius: 1rem;
+    margin-top: 2rem;
     margin-bottom: 2rem;
+    font-size: 1rem;
     font-weight: bold;
-    transition-duration: 0.4s;
+    transition: 0.3s ease;
 }
 
 button:hover {
@@ -104,14 +128,48 @@ button:hover {
 }
 
 
-
-button.vrai-faux {
-    background-color: #3C7196;
-
-}
-
 .boutons {
     display: flex;
     justify-content: space-around;
+
 }
+
+.card {
+    background-color: white;
+    box-shadow: 0.3rem 0.2rem 0.5rem 0rem rgba(0, 0, 0, 0.4);
+    border-radius: 1rem;
+    height: 40rem;
+    margin: 1rem;
+    margin-top: 0;
+    padding: 2rem;
+    padding-bottom: 6rem;
+    text-align: center;
+
+}
+
+.playing {
+    background-color: #8DB0C9;
+    height: 100%;
+    width: 100%;
+    padding-top: 3rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+    transition: 0.3s ease;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+}
+
+.wrong {
+    background-color: #FC9E8B;
+}
+.content{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+}
+
+
 </style>
